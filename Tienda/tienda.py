10b4fdf9eaa72dedd7062dc8b19d224a61900f21
Andres Grijalva <38090199+paquisha@users.noreply.github.com>
2022-07-13 16:17:15 -0500

    Update tienda.py
    cambio por tema cumplir parametros de documento

--- Tienda/tienda.py
+++ Tienda/tienda.py
@@ -1,22 +1,122 @@
-#menu
+import sys
+nombres = []
+edades = []
+tallas = []
+residencias = []
+emails = []
+precios = []
+cantidades_vendidas = []
 
-import string
+while True:
+    print("""
+=========================
+    sistema ventas
+=========================
+""")
+    eleccion = input("""
+1 - Ingrese datos
+2 - Hacer una venta
+3 - Mostrar información
+4 - Borrar un artículo
+5 - Salir
+Seleccione: """)
+    if eleccion == "1":
+        nombre = input("Ingrese nombre: ")
+        edad = input("Ingrese edad: ")
+        talla = input("Ingrese tallas: ")
+        residencia = input("Ingrese lugar de residencia: ")
+        email = input("Ingrese email: ")
+        precio = float(input("Precio del producto: "))
+        cantidad_vendida = 0.0
+        nombres.append(nombre)
+        edades.append(edad)
+        tallas.append(talla)
+        residencias.append(residencia)
+        emails.append(email)
+        precios.append(precio)
+        cantidades_vendidas.append(cantidad_vendida)
+    elif eleccion == "2":
+        nombre_articulo = input("Nombre del artículo que se vende: ")
+        if nombre_articulo in nombres:
+            cantidad = float(input("Cantidad vendida: "))
+            indice = nombres.index(nombre_articulo)
+            precio = precios[indice]
+            cantidades_vendidas[indice] += cantidad
+            print(
+                f"Se vende(n) {cantidad} {nombre_articulo}. Total: {cantidad * precio}")
+        else:
+            print("El artículo no existe")
+    elif eleccion == "3":
+        if len(nombres) <= 0:
+            print("No hay artículos")
+            continue
+        # Los nombres de artículos
+        articulo_mas_vendido = nombres[0]
+        articulo_menos_vendido = nombres[0]
+        articulo_con_mas_ingresos = nombres[0]
+        articulo_con_menos_ingresos = nombres[0]
+        # Pero también necesitamos el conteo. Simplemente los inicializamos en un elemento del arreglo
+        mas_vendido = cantidades_vendidas[0]
+        menos_vendido = cantidades_vendidas[0]
+        con_mas_ingresos = cantidades_vendidas[0] * precios[0]
+        con_menos_ingresos = cantidades_vendidas[0] * precios[0]
+        print("+--------------------+---------+---------+--------------------+--------------------+----------+----------+----------+")
+        print("|NOMBRE              |EDAD     |TALLA    |RESIDENCIA          |EMAIL               |CANT.     |PRECIO    |IMPORTE   |")
+        print("+--------------------+---------+---------+--------------------+--------------------+----------+----------+----------+")
+        indice = 0
+        total = 0
+        while indice < len(nombres):
+            nombre = nombres[indice]
+            edad = edades[indice]
+            talla = tallas[indice]
+            residencia = residencias[indice]
+            email = emails[indice]
+            precio = precios[indice]
+            cantidad_vendida = cantidades_vendidas[indice]
+            importe = precio * cantidad_vendida
+            print("|{:<20}|{:>10}|{:>10}{:<20}|{:<20}|{:>10.2f}|{:>10.2f}|{:>10.2f}|".format(
+                nombre, edad, talla, residencia, email, cantidad_vendida, precio, importe))
+            print("+--------------------+----------+----------+----------+")
+            if cantidad_vendida > mas_vendido:
+                mas_vendido = cantidad_vendida
+                articulo_mas_vendido = nombre
+            if cantidad_vendida < menos_vendido:
+                menos_vendido = cantidad_vendida
+                articulo_menos_vendido = nombre
+            if importe > con_mas_ingresos:
+                con_mas_ingresos = importe
+                articulo_con_mas_ingresos = nombre
+            if importe < con_menos_ingresos:
+                con_menos_ingresos = importe
+                articulo_con_menos_ingresos = nombre
+            total += importe
+            indice += 1
+
+        print(
+            "|--------------------|----------|----------|--------------------|--------------------|----------|----------|----------|TOTAL:    |{:>10.2f}|".format(total))
+        print("+--------------------+----------+----------+--------------------+--------------------+----------+----------+----------+")
+        print(
+            f"Artículo más vendido: {articulo_mas_vendido}, con {mas_vendido} unidades")
+        print(
+            f"Artículo menos vendido: {articulo_menos_vendido}, con {menos_vendido} unidades")
+        print(
+            f"Artículo con más ingresos: {articulo_con_mas_ingresos}, con {con_mas_ingresos} euros")
+        print(
+            f"Artículo con menos ingresos: {articulo_con_menos_ingresos}, con {con_menos_ingresos} euros")
+    elif eleccion == "4":
+        nombre_articulo = input("Nombre del artículo que se elimina: ")
+        if nombre_articulo in nombres:
+            indice = nombres.index(nombre_articulo)
+            del nombres[indice]
+            del precios[indice]
+            del cantidades_vendidas[indice]
+            print(f"Se elimina {nombre_articulo}")
+        else:
+            print("El artículo no existe")
 
 
-menuPrincipal = int(input("Menu Principal \n 1- opcion uno  \n 2- opcion dos \n 3-opcion tres \n 4- opcion cuatro \n 0- salir"))
+    elif eleccion == "5":
+        if input("Seguro (s/n): ") == "s":
+            sys.exit()
 
-while menuPrincipal != 0:
-    if menuPrincipal == 1:
-        nombre = input('ingrese nombre:')
-        print(nombre)
-    elif menuPrincipal == 2:
-        print('dos')
-    elif menuPrincipal == 3:
-        print('tres')
-    elif menuPrincipal == 4:
-        print('cuatro')
-    else:
-        print('pro favor ingrese opcion correcta')
-    
-    menuPrincipal = int(input("Menu Principal \n 1- opcion uno  \n 2- opcion dos \n 3-opcion tres \n 4- opcion cuatro \n 0- salir"))
     
